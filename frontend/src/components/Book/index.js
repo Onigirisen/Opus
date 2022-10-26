82231c70395e64a2515fae28a3b85e90d53cb644
Avisek Pandit <simonp4466@gmail.com>
2022-10-26 19:50:32 -0400

    add arrow button on flipping pages

--- frontend/src/components/Book/index.js
+++ frontend/src/components/Book/index.js
@@ -36,15 +36,19 @@ const BookComponent = () => {
                         <>
                             <div className="fake-right-back-page"></div> 
                             <div className="fake-right-page">
-                                <textarea className="book-page-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                                <textarea className="book-page-content"></textarea>
                                 <div className="book-page-number">50</div>
+                                <div className="prev-page">{"<"}</div>
+                                <div className="next-page">{">"}</div>
                             </div>
                         </> :  
                         <>
                         <div className="fake-left-back-page"></div> 
                         <div className="fake-left-page">
-                            <textarea className="book-page-content">ZZZLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                            <textarea className="book-page-content"></textarea>
                             <div className="book-page-number">50</div>
+                            <div className="prev-page">{"<"}</div>
+                            <div className="next-page">{">"}</div>
                         </div>
                         </>  : 
                     <></>
