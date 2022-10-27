7abc64a6f8acca6f6ebbd55d268850705d1537b1
Chun Lau <dlaucodes@gmail.com>
2022-10-27 19:55:23 -0400

    working faces

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -1,29 +1,69 @@
 import React from "react";
-import Flickity from 'react-flickity-component'
-import './carousel.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './carousel1.css'
+import carouselpic from "../../assets/profile/goat.jpeg"
+import carouselpic2 from "../../assets/profile/goat.jpeg"
+import carouselpic3 from "../../assets/profile/goat.jpeg"
+import Carousel from 'react-bootstrap/Carousel'
 
-const flickityOptions = {
-    initialIndex: 2
-}
-
-const Carousel= ()=> {
+function OwnCarousel () {
   return (
-    <Flickity
-      className={'carousel'} // default ''
-      elementType={'div'} // default 'div'
-      options={flickityOptions} // takes flickity options {}
-      disableImagesLoaded={false} // default false
-      reloadOnUpdate // default false
-      static // default false
-    >
+      <>
+      <div className="carousel-container">
+
     
-      <div className="gallery-cell">hello</div>
-      <div className="gallery-cell"></div>
-      <div className="gallery-cell"></div>
-      <div className="gallery-cell"></div>
-      <div className="gallery-cell"></div>
-    </Flickity>
-  )
+    <Carousel>
+      <Carousel.Item >
+         
+        <div className="img-container">
+            <img className="d-block w-100" src={carouselpic} alt="First slide" />
+  
+            <img className="d-block w-100" src={carouselpic2} alt="Second slide"/>
+            
+            <img className="d-block w-100" src={carouselpic3} alt="Third slide"/>
+        </div>
+
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    
+      <Carousel.Item>
+        <div className="img-container">
+            <img className="d-block w-100" src={carouselpic} alt="First slide" />
+  
+            <img className="d-block w-100" src={carouselpic2} alt="Second slide"/>
+            
+            <img className="d-block w-100" src={carouselpic3} alt="Third slide"/>
+        </div>
+       
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+         <div className="img-container">
+            <img className="d-block w-100" src={carouselpic} alt="First slide" />
+  
+            <img className="d-block w-100" src={carouselpic2} alt="Second slide"/>
+            
+            <img className="d-block w-100" src={carouselpic3} alt="Third slide"/>
+        </div>
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+    </>
+  );
 }
 
-export default Carousel;
+export default OwnCarousel;
