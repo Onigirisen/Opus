0d541a49e44275aa52d5d9fc4b0af05bbf5c424c
Ryan Kok <3nayrkok5@gmail.com>
2022-10-27 17:40:42 -0400

    changed userprofile file name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-import { Switch } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute } from './components/Routes/routes';
-import NavBar from './components/NavBar';
-import SplashPage from './components/SplashPage/SplashPage';
-import UserProfile from './components/userProfile';
-import BookComponent from './components/Book';
-import CreateBookPage from './components/CreateBookPage';
+import { Switch } from "react-router-dom";
+import { AuthRoute, ProtectedRoute } from "./components/Routes/routes";
+import NavBar from "./components/NavBar";
+import SplashPage from "./components/SplashPage/SplashPage";
+import UserProfile from "./components/UserProfile";
+import BookComponent from "./components/Book";
+import CreateBookPage from "./components/CreateBookPage";
 
-import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { getCurrentUser } from './store/session';
-import { Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Carousel from './components/Carousel';
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { getCurrentUser } from "./store/session";
+import { Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Carousel from "./components/Carousel";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -20,17 +20,19 @@ function App() {
     dispatch(getCurrentUser()).then(() => setLoaded(true));
   }, [dispatch]);
 
-  return loaded && (
-    <>
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={SplashPage} />
-        <ProtectedRoute exact path="/profile" component={UserProfile} />
-        <Route exact path="/book" component={BookComponent} />
-        <Route exact path="/book/create" component={CreateBookPage} />
-      </Switch>
-      <Footer />
-    </>
+  return (
+    loaded && (
+      <>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={SplashPage} />
+          <ProtectedRoute exact path="/profile" component={UserProfile} />
+          <Route exact path="/book" component={BookComponent} />
+          <Route exact path="/book/create" component={CreateBookPage} />
+        </Switch>
+        <Footer />
+      </>
+    )
   );
 }
 
