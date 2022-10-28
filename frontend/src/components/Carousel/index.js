9875ba6ee4950bc82b63fc207ad5fa02c411fff4
Chun Lau <dlaucodes@gmail.com>
2022-10-27 21:03:13 -0400

    carouselcomplete

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './carousel1.css'
-import carouselpic from "../../assets/profile/goat.jpeg"
-import carouselpic2 from "../../assets/profile/goat.jpeg"
-import carouselpic3 from "../../assets/profile/goat.jpeg"
+import carouselpic from "../../assets/notes/notes.webp"
+import carouselpic2 from "../../assets/notes/sspage.png"
+import carouselpic3 from "../../assets/notes/scribbles.jpeg"
+import carouselpic4 from "../../assets/notes/notes.webp"
+import carouselpic5 from "../../assets/notes/page.jpeg"
+import carouselpic6 from "../../assets/notes/sspage2.png"
+import carouselpic7 from "../../assets/notes/handwritten.jpeg"
+import carouselpic8 from "../../assets/notes/sspage.png"
+import carouselpic9 from "../../assets/notes/scribbles.jpeg"
 import Carousel from 'react-bootstrap/Carousel'
 
 function OwnCarousel () {
@@ -16,48 +22,52 @@ function OwnCarousel () {
       <Carousel.Item >
          
         <div className="img-container">
+           
             <img className="d-block w-100" src={carouselpic} alt="First slide" />
-  
+   
+      
             <img className="d-block w-100" src={carouselpic2} alt="Second slide"/>
-            
+        
+    
             <img className="d-block w-100" src={carouselpic3} alt="Third slide"/>
+          
         </div>
 
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          {/* <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
     
       <Carousel.Item>
         <div className="img-container">
-            <img className="d-block w-100" src={carouselpic} alt="First slide" />
+            <img className="d-block w-100" src={carouselpic4} alt="First slide" />
   
-            <img className="d-block w-100" src={carouselpic2} alt="Second slide"/>
+            <img className="d-block w-100" src={carouselpic5} alt="Second slide"/>
             
-            <img className="d-block w-100" src={carouselpic3} alt="Third slide"/>
+            <img className="d-block w-100" src={carouselpic6} alt="Third slide"/>
         </div>
        
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          {/* <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
          <div className="img-container">
-            <img className="d-block w-100" src={carouselpic} alt="First slide" />
+            <img className="d-block w-100" src={carouselpic7} alt="First slide" />
   
-            <img className="d-block w-100" src={carouselpic2} alt="Second slide"/>
+            <img className="d-block w-100" src={carouselpic8} alt="Second slide"/>
             
-            <img className="d-block w-100" src={carouselpic3} alt="Third slide"/>
+            <img className="d-block w-100" src={carouselpic9} alt="Third slide"/>
         </div>
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          {/* <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          </p> */}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
