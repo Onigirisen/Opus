d1925c60bddcf27fcafec97a30ef838ff4a2d2b7
Ryan Kok <3nayrkok5@gmail.com>
2022-10-31 03:46:00 -0400

    cleaned up all unused variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,18 @@
-import { Switch } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute } from './components/Routes/routes';
-import NavBar from './components/NavBar';
-import SplashPage from './components/SplashPage/SplashPage';
-import UserProfile from './components/UserProfilePage/index';
-import BookComponent from './components/Book';
-import CreateBookPage from './components/CreateBookPage';
+import { Switch } from "react-router-dom";
+import { ProtectedRoute } from "./components/Routes/routes";
+import NavBar from "./components/NavBar";
+import SplashPage from "./components/SplashPage/SplashPage";
+import UserProfile from "./components/UserProfilePage/index";
+import BookComponent from "./components/Book";
+import CreateBookPage from "./components/CreateBookPage";
 // import CreateChapterPage from './components/CreateChapterPage';
-import BooksIndex from './components/BookIndex';
-import BookShow from './components/BookShow';
-import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { getCurrentUser } from './store/session';
-import { Route } from 'react-router-dom';
-import Footer from './components/Footer';
-
-
+import BooksIndex from "./components/BookIndex";
+import BookShow from "./components/BookShow";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { getCurrentUser } from "./store/session";
+import { Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -23,20 +21,26 @@ function App() {
     dispatch(getCurrentUser()).then(() => setLoaded(true));
   }, [dispatch]);
 
-  return loaded && (
-    <>
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={SplashPage} />
-        <ProtectedRoute exact path="/profile" component={UserProfile} />
-        <Route exact path="/books" component={BooksIndex} />
-        <Route exact path="/books/read" component={BookComponent}></Route>
-        <Route exact path="/books/:bookId" component={BookShow} />
-        <ProtectedRoute exact path="/book/create" component={CreateBookPage} />
-      </Switch>
-      <Footer />
-    </>
-  )
+  return (
+    loaded && (
+      <>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={SplashPage} />
+          <ProtectedRoute exact path="/profile" component={UserProfile} />
+          <Route exact path="/books" component={BooksIndex} />
+          <Route exact path="/books/read" component={BookComponent}></Route>
+          <Route exact path="/books/:bookId" component={BookShow} />
+          <ProtectedRoute
+            exact
+            path="/book/create"
+            component={CreateBookPage}
+          />
+        </Switch>
+        <Footer />
+      </>
+    )
+  );
 }
 
 export default App;
--- frontend/src/components/BookShow/index.js
+++ frontend/src/components/BookShow/index.js
@@ -1,52 +1,70 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
-import Modal from '../../Modal/Modal'
+// import Modal from '../../Modal/Modal'
 import { fetchBook } from "../../store/books";
-import './BookShow.css'
+import "./BookShow.css";
 
 const BookShow = () => {
-    const dispatch = useDispatch()
-    const history = useHistory()
-    const [loaded, setLoaded] = useState(false)
-    const books = useSelector(state => state.books)
-    const { bookId } = useParams()
-    const book = books[bookId];
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [loaded, setLoaded] = useState(false);
+  const books = useSelector((state) => state.books);
+  const { bookId } = useParams();
+  const book = books[bookId];
 
-    useEffect(() => {
-        dispatch(fetchBook(bookId)).then(() => setLoaded(true))
-    }, [])
+  useEffect(() => {
+    dispatch(fetchBook(bookId)).then(() => setLoaded(true));
+  }, []);
 
-    return loaded && (
-        <div className="create-book-container">
-            <div className="create-book-cover" style={{backgroundColor: book.coverColor}}>
-                <div className="create-book-spine">
-
-                </div>
-                <div className="create-book-text-container">
-                    <div className="create-book-title">
-                        {book.title}
-                    </div>
-                    <div className="create-book-genre">
-                        genre: {book.genre}
-                    </div>
-                    <div className="create-book-author">
-                        Author: {book.user}
-                    </div>
-                </div>
-            </div>
-            <div className="create-book-form">
-                <div className="create-book-title-bio-container">
-                    <input type="text" className="create-book-title-text" spellCheck="false" value={book.title} readOnly/>
-                    <div className="create-book-genre-div">genre</div>
-                    <input type="text" className="create-book-genre-text" spellCheck="false" value={book.genre} readOnly/>
-                    <div className="create-book-description-text">Description</div>
-                    <textarea className="create-book-description" spellCheck="false" readOnly></textarea>
-                </div>
-                    <button className="book-show-read-button" onClick={() => history.push(`/books/read`)}>Read Book</button>
-            </div>
+  return (
+    loaded && (
+      <div className="create-book-container">
+        <div
+          className="create-book-cover"
+          style={{ backgroundColor: book.coverColor }}
+        >
+          <div className="create-book-spine"></div>
+          <div className="create-book-text-container">
+            <div className="create-book-title">{book.title}</div>
+            <div className="create-book-genre">genre: {book.genre}</div>
+            <div className="create-book-author">Author: {book.user}</div>
+          </div>
         </div>
+        <div className="create-book-form">
+          <div className="create-book-title-bio-container">
+            <input
+              type="text"
+              className="create-book-title-text"
+              spellCheck="false"
+              value={book.title}
+              readOnly
+            />
+            <div className="create-book-genre-div">genre</div>
+            <input
+              type="text"
+              className="create-book-genre-text"
+              spellCheck="false"
+              value={book.genre}
+              readOnly
+            />
+            <div className="create-book-description-text">Description</div>
+            <textarea
+              className="create-book-description"
+              spellCheck="false"
+              readOnly
+            ></textarea>
+          </div>
+          <button
+            className="book-show-read-button"
+            onClick={() => history.push(`/books/read`)}
+          >
+            Read Book
+          </button>
+        </div>
+      </div>
     )
-}
+  );
+};
 
 export default BookShow;
