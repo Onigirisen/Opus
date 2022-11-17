fa8ab6de784f9e619720df3aba5d139f4d4c1b8d
Avisek Pandit <simonp4466@gmail.com>
2022-11-17 13:24:12 -0500

    logic for chapters index showing

--- frontend/src/components/PageShow/index.js
+++ frontend/src/components/PageShow/index.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchChapters } from "../../store/chapters";
 import { fetchPages } from "../../store/pages";
-import BookComponent from "../Book";
 import './Page.css'
 
 const PageShow = () => {
@@ -11,6 +10,7 @@ const PageShow = () => {
     const chapters = useSelector((state) => state.chapters)
     const pages = useSelector((state) => state.pages)
     const [loaded, setLoaded] = useState(false);
+    const [chapterShowing, setChapterShowing] = useState();
     const { bookId, chapterId, pageId } = useParams();
 
     useEffect(() => {
@@ -24,20 +24,27 @@ const PageShow = () => {
     Object.values(chapters).forEach((chapter) => {if (chapter.book === bookId && !chaptersArr.includes(chapter)) chaptersArr.push(chapter)})
     Object.values(pages).forEach((page) => {if (page.chapter === chapterId) pagesArr.push(page)})
 
+    const pagesList = pagesArr.map((page) => (
+        <div className="page-show-page-container">
+            
+        </div>
+    ))
+
     const chaptersList = chaptersArr.map((chapter) => (
-        <div className="page-show-chapters-container">
+        <div className="page-show-chapter-container" key={chapter._id + "page"} onClick={() => {
+            chapterShowing === chapter._id ? setChapterShowing() : setChapterShowing(chapter._id)
+        }}>
             <div className="page-show-chapter-number">{chapter.chapterNumber}</div>
             <div className="page-show-chapter-title">{chapter.title}</div>
         </div>
     ))
 
-    return (
+    return loaded && (
         <>
         <div className="chapters-index-container">
             <div className="chapters-index-header">Chapters</div>
             {chaptersList}
         </div>
-        <BookComponent></BookComponent>
         </>
     );
 }
