2b926d30db8937bb7da7e0d54287ece262c14df7
Darian Chen <darianychen@gmail.com>
2022-11-17 23:53:09 -0500

    Start adding books to user profile

--- frontend/src/components/UserProfilePage/index.js
+++ frontend/src/components/UserProfilePage/index.js
@@ -6,38 +6,37 @@ import editCamera from '../../assets/profile/cameratrans.png'
 import { fetchUser, getUser, updateBio } from "../../store/users";
 import { useParams } from "react-router-dom";
 import { uploadPic } from "../../store/pics";
+import { fetchBooks } from "../../store/books";
 
 const UserProfile = () => {
     const { userId } = useParams();
     const dispatch = useDispatch();
     const user = useSelector(getUser(userId));
     const [bio, setBio] = useState(user ? user.bio : " ");
-    const [loaded, setLoaded] = useState(false);
+    const [bookLoaded, setBookLoaded] = useState(false);
+    const [userLoaded, setUserLoaded] = useState(false);
     const [buttonText, setButtonText] = useState("edit")
     const [editClicked, setEditClick] = useState(false);
     const [text, setText] = useState(user ? user.bio : " ")
     const displayingBio = useRef();
-    const [profilePic, setProfilePic] = useState(user ? user.profilePictureUrl : "");
+
+    const books = useSelector(({books}) => books ? () => {
+        let allBooks = [];
+        for (const id in books) {
+            if(books[id].user === userId){
+                allBooks.push(books[id]);
+            }
+          }
+          return allBooks;
+    } : [])()
     
- 
-    // useEffect(() => {
-    //     dispatch(fetchUser(userId))
-    // }, [])
+    useEffect(() => {
+        dispatch(fetchBooks()).then(() => setBookLoaded(true));
+      }, []);
 
     useEffect(() => {
-        dispatch(fetchUser(userId)).then(() => {
-            setLoaded(true);
-            displayingBio.current = true;
-        });
-    }, [dispatch])
-
-    useEffect(() => {
-        dispatch(fetchUser(userId))
-        if (displayingBio.current === true) {
-            setBio(user.bio);
-            setText(user.bio);
-        }
-    }, [displayingBio.current])
+        dispatch(fetchUser(userId)).then(() => setUserLoaded(true));
+    }, []);
 
 
     const handleClick = (e) => {
@@ -77,54 +76,56 @@ const UserProfile = () => {
     }
 
 
-    return loaded && (
+    return bookLoaded && userLoaded && (
         <>
-        <div className="body-container">
-        <div className="spacing-top">
-        </div>
-    <div className="profile-body-container">
-        <div className="profile-picture-wrapper">
-            <div className="profile-picture-container">
-                <img src={user.profilePictureUrl} alt="profile picture" />
-            </div>
-            <div className="profile-picture-edit-container">
-                <img src={editCamera} className="camera" alt="camera" onClick={cameraClick}/>
-                <input className="cameraInput" type="file" onChange={picSubmit}/>
-            </div>
-        </div>
-            <div className="profile-bio-wrapper">
-                <div className="profile-bio-container">
-                    <div className="profile-bio-heading-container">
-                        <h2>{user.username}</h2>
-                            <div className="profile-edit-button-container">
-                                <button className="profile-edit-button" onClick={picSubmit}>
-                                    {buttonText}
-                                </button>
+            <div className="body-container">
+                <div className="spacing-top"></div>
+                <div className="profile-body-container">
+                    <div className="profile-picture-wrapper">
+                        <div className="profile-picture-container">
+                    {/* <img src={user.profilePictureUrl} alt="profile picture" /> */}
+                    </div>
+                <div className="profile-picture-edit-container">
+                    <img src={editCamera} className="camera" alt="camera" onClick={cameraClick}/>
+                    <input className="cameraInput" type="file" onChange={picSubmit}/>
+                </div>
+                </div>
+                <div className="profile-bio-wrapper">
+                    <div className="profile-bio-container">
+                        <div className="profile-bio-heading-container">
+                            
+                                <div className="profile-edit-button-container">
+                                    <button className="profile-edit-button" onClick={picSubmit}>
+                                        {buttonText}
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    <div className="profile-bio-text-container">
-                    { text ? text : "" }
+                        <div className="profile-bio-text-container">
+                        { text ? text : "" }
 
-                    { editClicked ? 
-                        <form onSubmit={handleSubmit}>
-                            <textarea className="profile_edit_textarea" 
-                                value={bio}
-                                onChange={e => setBio(e.target.value)}>
-                            </textarea>
-                            <button type="submit">update bio</button>
-                        </form> :
-                         ''}
+                        { editClicked ? 
+                            <form onSubmit={handleSubmit}>
+                                <textarea className="profile_edit_textarea" 
+                                    // value={bio}
+                                    onChange={e => setBio(e.target.value)}>
+                                </textarea>
+                                <button type="submit">update bio</button>
+                            </form> :
+                            ''}
+                        </div>
+                    </div>
+                    </div>
                     </div>
                 </div>
-            </div>
- 
-        </div>
-            <div className="spacing-top"></div>
-        </div>
-        <div className="user-profile-books">
-            <div className="user-profile-select-books">All | Public | Private </div>
-        </div>
-    </>
+                <div className="user-profile-books">
+                    <div className="books-content">
+                        <div className="user-profile-select-books">All | Public | Private </div>
+                        <div className="books">
+                            {books[0].title}
+                        </div>
+                    </div>
+                </div>
+        </>
     )
 }
 
