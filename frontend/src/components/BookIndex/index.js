fe8fac005ed0123a1bae6147be98873f72b3b182
Avisek Pandit <simonp4466@gmail.com>
2022-12-06 00:50:18 -0500

    addedsearchbar

--- frontend/src/components/BookIndex/index.js
+++ frontend/src/components/BookIndex/index.js
@@ -11,6 +11,7 @@ const BooksIndex = () => {
   const history = useHistory();
   const users = useSelector(getUsers);
   const [loaded, setLoaded] = useState(false);
+  const [searchContent, setSearchContent] = useState('');
   const books = useSelector((state) => state.books);
   const testDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
@@ -53,6 +54,11 @@ const BooksIndex = () => {
   return (
     loaded && (
       <>
+        <div className="books-index-search">
+          <form className="books-index-search-form">
+            <input className="books-index-search-input" type="text" placeholder="Search" value={searchContent} onChange={(e) => {setSearchContent(e.target.value)}} />
+          </form>
+        </div>
         <div className="books-index-container">{booksList}</div>
       </>
     )
